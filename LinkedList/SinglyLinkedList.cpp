8a9406f82ea2b3b924fdb84da98e99f50d8ab1b1
paras nayal <parasnayal955@gmail.com>
2023-06-10 23:40:40 +0530

    create singly linked list and performing operation insertion and deletion

--- LinkedList/SinglyLinkedList.cpp
+++ LinkedList/SinglyLinkedList.cpp
@@ -1,6 +1,7 @@
 // Creation of the singly linked list
 #include <iostream>
 using namespace std;
+
 class Node
 {
 public:
@@ -12,6 +13,7 @@ public:
         this->next = NULL;
     }
 };
+
 class list
 {
 private:
@@ -23,51 +25,69 @@ public:
         this->head = NULL;
         this->tail = NULL;
     }
+
     void create_list();
     void insert_at_beg();
     void insert_at_end();
     void insert_at_index(int index);
+    void delete_at_beg();
+    void delete_at_end();
+    void delete_at_index(int index);
     void display();
+    int getLength()
+    {
+        int length = 0;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            temp = temp->next;
+            length++;
+        }
+        return length;
+    }
 };
+
 void list ::create_list()
 {
     int data, choice = 1;
-    Node *temp, *newNode;
+    Node *newNode, *temp;
     while (choice)
     {
-        cout << "Enter the element of the linked list " << endl;
+        cout << "Enter the element" << endl;
         cin >> data;
         newNode = new Node(data);
         if (head == NULL)
         {
-            head = temp = tail = newNode;
+            temp = head = tail = newNode;
         }
         else
         {
             temp->next = newNode;
             temp = newNode;
-            tail = newNode;
+            tail = temp;
         }
         cout << "Do you want to continue (0 , 1 ) ? " << endl;
         cin >> choice;
     }
 }
+
 void list ::insert_at_beg()
 {
     int data, choice = 1;
     Node *temp = head, *newNode;
     while (choice)
     {
-        cout << "Enter element at the beginning of the linked list " << endl;
+        cout << "Enter the element at the beginning of the linked list " << endl;
         cin >> data;
         newNode = new Node(data);
         newNode->next = temp;
         temp = newNode;
-        cout << "Do you want to continue (0 , 1 ) ? " << endl;
+        cout << "Do you want to continue ( 0 , 1 ) ? " << endl;
         cin >> choice;
     }
     head = temp;
 }
+
 void list ::insert_at_end()
 {
     int data, choice = 1;
@@ -79,26 +99,70 @@ void list ::insert_at_end()
         newNode = new Node(data);
         temp->next = newNode;
         temp = newNode;
-        cout << "Do you want to continue (0 , 1 ) ? " << endl;
+        cout << "Do you want to continue ( 0 , 1 ) ? " << endl;
         cin >> choice;
     }
     tail = temp;
 }
+
 void list ::insert_at_index(int index)
 {
     int data, i = 0;
     Node *temp = head, *newNode;
-
-    while (i < index-1)
+    if (index == 0)
     {
-        temp = temp->next;
+        insert_at_beg();
+    }
+    else if (index == getLength())
+    {
+        insert_at_end();
+    }
+    else
+    {
+        while (i < index - 1)
+        {
+            temp = temp->next;
+            i++;
+        }
+        cout << "Enter the data at the index " << index << endl;
+        cin >> data;
+        newNode = new Node(data);
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void list ::delete_at_beg()
+{
+    Node *temp = head;
+    head = temp->next;
+    delete temp;
+}
+
+void list ::delete_at_end()
+{
+    Node *temp = tail, *prevNode = head;
+    while (prevNode->next != tail)
+    {
+        prevNode = prevNode->next;
+    }
+    prevNode->next = NULL;
+    tail = prevNode;
+    delete temp;
+}
+
+void list ::delete_at_index(int index)
+{
+    int i = 0;
+    Node *nextNode = head->next, *curNode = head;
+    while (i < index - 1)
+    {
+        curNode = curNode->next;
+        nextNode = nextNode->next;
         i++;
     }
-    cout << "Enter the element " << endl;
-    cin >> data;
-    newNode = new Node(data);
-    newNode->next = temp->next;
-    temp->next = newNode;
+    curNode->next = nextNode->next;
+    delete nextNode;
 }
 void list ::display()
 {
@@ -114,20 +178,38 @@ int main(void)
 {
     list l1;
     l1.create_list();
-    cout << "Elements of the linked list are => " << endl;
+    cout << "Elements of the singly linked list are => " << endl;
     l1.display();
 
     l1.insert_at_beg();
-    cout << "Elements of the linked list after inserting the element at beginning => " << endl;
+    cout << "Elements of the singly linked list after inserting element at the beginning => " << endl;
     l1.display();
 
     l1.insert_at_end();
-    cout << "Elements of the linked list after inserting the element at the end => " << endl;
+    cout << "Elements of the singly linked list after inserting element at the end => " << endl;
     l1.display();
 
     int index = 3;
     l1.insert_at_index(index);
-    cout << "Elements of the linked list after inserting the element at index " << index << " are => " << endl;
+    cout << "Elements of the singly linked list after inserting element at the index " << index << " are => " << endl;
     l1.display();
+
+    cout << "Length of the linked list is => " << l1.getLength() << endl;
+
+    l1.delete_at_beg();
+    cout << "Elements of the singly linked list after deleting the element from the beginning => " << endl;
+    l1.display();
+
+    l1.delete_at_end();
+    cout << "Elements of the singly linked list after deleting the element from the end => " << endl;
+    l1.display();
+
+    index = 2;
+    l1.delete_at_index(index);
+    cout << "Elements of the singly linked list after deleting the element from the index " << index << " are => " << endl;
+    l1.display();
+
+    cout << "Length of the linked list is => " << l1.getLength() << endl;
+
     return 0;
 }
