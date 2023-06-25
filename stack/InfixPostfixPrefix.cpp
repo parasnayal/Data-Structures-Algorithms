aaf333ec4785b9e6e1ab3be4c3110b1025aa81e4
paras nayal <parasnayal955@gmail.com>
2023-06-25 18:33:59 +0530

    Introduction to the infix , postfix and prefix expression

---
+++ stack/InfixPostfixPrefix.cpp
@@ -0,0 +1,8 @@
+// Infix => Infix is the way to write a expression In which operator is written between the operand.It can easily understand by the human.
+// Example => a+b
+
+// Prefix => Is the way to write a expression in which operator is written before operand .It is also known as polish notation.
+// Example => +ab
+
+// Postfix => Is the way to write a expression in which operator is written after the operand .It is also known as Reverse polish notation.
+// Example => ab+
